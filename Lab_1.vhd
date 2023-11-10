efbe6cc9aa9eca0e93985715686f393cccf956f9
Khoded14 <88177462+Khoded14@users.noreply.github.com>
2023-11-09 18:33:20 -0600

    Add files via upload

---
+++ Lab_1.vhd
@@ -0,0 +1,25 @@
+-- Code 
+
+entity thermo is 
+port (current_temp : in bit_vector(6 downto 0); 
+      desired_temp : in bit_vector(6 downto 0); 
+      display_sel  : in bit; 
+      display_temp : out bit_vector(6 downto 0)); 
+
+end thermo; 
+
+architecture test of thermo is 
+begin 
+
+process(current_temp,desired_temp,display_sel)
+begin 
+
+if display_sel = '1' then 
+display_temp <= current_temp; 
+
+else 
+display_temp <= desired_temp; 
+
+end if; 
+end process; 
+end test; 
---
+++ Test_Lab1.vhd
@@ -0,0 +1,37 @@
+entity T_thermo is 
+end T_thermo; 
+
+architecture test of T_thermo is 
+
+component thermo
+port (current_temp : in bit_vector(6 downto 0); 
+      desired_temp : in bit_vector(6 downto 0); 
+      display_sel  : in bit; 
+      display_temp : out bit_vector(6 downto 0));
+end component;
+      
+signal current_temp, desired_temp : bit_vector(6 downto 0);
+signal display_temp : bit_vector(6 downto 0);
+signal display_sel : bit;
+
+begin
+UUT: thermo port map (current_temp => current_temp,
+             desired_temp => desired_temp, 
+             display_sel => display_sel, 
+             display_temp => display_temp);
+                         
+process -- no sensitivity list
+begin
+current_temp <= "1010101"; 
+desired_temp <= "0101010"; 
+display_sel  <= '0'; 
+wait for 200 ns;
+display_sel <= '1';
+wait for 200 ns;
+display_sel  <= '0'; 
+wait for 200 ns;
+display_sel <= '1';
+wait for 200 ns;
+wait; 
+end process;
+end test;  
